da1dfc4ea5d0daeb0b6fb8b2c15c07603e6286ed
unknown <kacper.leszczu8@gmail.com>
2023-05-28 01:51:10 +0200

    changed login

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2")
 
 module.exports = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME
+    host: 'bvjx9vxnnapywt8oi7oh-mysql.services.clever-cloud.com',
+    user: 'utpprvmh23np6jwe',
+    password: 'tcOqn2tDVFbd36xtj8o2',
+    database: 'bvjx9vxnnapywt8oi7oh'
 })
