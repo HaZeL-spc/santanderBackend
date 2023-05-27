86bf0023bf0d27b4940361b20610a81d3d89ee31
unknown <kacper.leszczu8@gmail.com>
2023-05-28 01:21:04 +0200

    updated credentials to sql

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2")
 
 module.exports = mysql.createConnection({
-    host: 'localhost',
-    user: 'kacper',
-    password: '1234',
-    database: 'santander'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME
 })
