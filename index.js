ee515874b17eee45d22d7b4217448050d2d304a8
unknown <kacper.leszczu8@gmail.com>
2023-05-28 02:30:53 +0200

    testing

--- index.js
+++ index.js
@@ -1,34 +1,74 @@
-// const express = require("express")
-// const app = express();
-// const path = require("path")
+// // const express = require("express")
+// // const app = express();
+// // const path = require("path")
+// // const serverRoute = require("./serverRoute")
+
+// // app.use(express.json())
+// // app.use(express.urlencoded({extended: false}))
+// // app.use(express.static(path.join(__dirname + "/public")))
+
+// // app.use("/api/", serverRoute);
+
+// // const PORT = process.env.PORT || 5000
+
+// // app.listen(PORT, "0.0.0.0", () => {
+// //     console.log("server started on 5000")
+// // })
+
+// // const express = require("express")
+// const app = express()
+
+// // require('dotenv').config()
+
+// app.use(express.urlencoded({extended: false}))
+// app.use(express.json())
+
 // const serverRoute = require("./serverRoute")
 
-// app.use(express.json())
-// app.use(express.urlencoded({extended: false}))
-// app.use(express.static(path.join(__dirname + "/public")))
-
-// app.use("/api/", serverRoute);
+// app.use("/api", serverRoute)
 
 // const PORT = process.env.PORT || 5000
 
-// app.listen(PORT, "0.0.0.0", () => {
-//     console.log("server started on 5000")
+// app.listen(PORT, () => {
+//     console.log("Server is running....")
 // })
 
-const express = require("express")
-const app = express()
+const express = require("express");
+const { Router } = require("express");
+const router = Router();
+const db = require("./database");
 
-// require('dotenv').config()
+router.use((req, res, next) => {
+  console.log("request made to server route");
+  next();
+});
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+router.get("/", async (req, res) => {
+  try {
+    // Create a new MySQL connection instance
+    const connection = mysql.createConnection({
+      host: 'bvjx9vxnnapywt8oi7oh-mysql.services.clever-cloud.com',
+      user: 'utpprvmh23np6jwe',
+      password: 'tcOqn2tDVFbd36xtj8o2',
+      database: 'bvjx9vxnnapywt8oi7oh',
+      port: 3306
+    });
 
-const serverRoute = require("./serverRoute")
+    // Connect to the MySQL server
+    await connection.connect();
 
-app.use("/api", serverRoute)
+    // Execute your query
+    const query = "SELECT S.id, G.nazwa as nazwaGrupy, S.imie, S.nazwa as nazwisko FROM STUDENT S LEFT JOIN GRUPA G ON S.id_grupa = G.id";
+    const [results] = await connection.promise().query(query);
 
-const PORT = process.env.PORT || 5000
+    // Close the connection
+    connection.end();
 
-app.listen(PORT, () => {
-    console.log("Server is running....")
-})
+    res.status(200).send(results);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+module.exports = router;
