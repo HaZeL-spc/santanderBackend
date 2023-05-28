40a781fcfb5869879aa94dc9817ca6d0e47f7deb
unknown <kacper.leszczu8@gmail.com>
2023-05-28 03:01:44 +0200

    added public folder

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const serverRoute = require("./serverRoute")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-// app.use(express.static(path.join(__dirname + "/public")))
+app.use(express.static(path.join(__dirname + "/public")))
 
 app.use("/api/", serverRoute);
 
