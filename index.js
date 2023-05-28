c1ec4c18daeef6e5024abca3e0d7efb30d49a7c8
unknown <kacper.leszczu8@gmail.com>
2023-05-28 12:43:10 +0200

    adjusted history in server

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express")
 const app = express();
 const path = require("path")
+const history = require('express-history-api-fallback');
 const serverRoute = require("./serverRoute")
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname + "/public")))
 
 app.use("/api/", serverRoute);
+const root = `${__dirname}/public`;
+app.use(history('index.html', { root }));
 
 const PORT = process.env.PORT || 5000
 
