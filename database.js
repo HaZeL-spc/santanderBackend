3b3c649f9f68fc27ac099a9186c1273d5f8cadc8
unknown <kacper.leszczu8@gmail.com>
2023-05-28 03:06:11 +0200

    added process.env when connecting db

--- database.js
+++ database.js
@@ -1,9 +1,17 @@
 const mysql = require("mysql2")
 
+// module.exports = mysql.createConnection({
+//     host: 'bvjx9vxnnapywt8oi7oh-mysql.services.clever-cloud.com',
+//     user: 'utpprvmh23np6jwe',
+//     password: 'tcOqn2tDVFbd36xtj8o2',
+//     database: 'bvjx9vxnnapywt8oi7oh',
+//     port: 3306
+// })
+
 module.exports = mysql.createConnection({
-    host: 'bvjx9vxnnapywt8oi7oh-mysql.services.clever-cloud.com',
-    user: 'utpprvmh23np6jwe',
-    password: 'tcOqn2tDVFbd36xtj8o2',
-    database: 'bvjx9vxnnapywt8oi7oh',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
     port: 3306
 })
