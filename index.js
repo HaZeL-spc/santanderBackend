4ebc879479dc151f31594ecb31741bbb7cd0e143
unknown <kacper.leszczu8@gmail.com>
2023-05-28 02:08:40 +0200

    testing

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
+// const express = require("express")
+// const app = express();
+// const path = require("path")
+// const serverRoute = require("./serverRoute")
+
+// app.use(express.json())
+// app.use(express.urlencoded({extended: false}))
+// app.use(express.static(path.join(__dirname + "/public")))
+
+// app.use("/api/", serverRoute);
+
+// const PORT = process.env.PORT || 5000
+
+// app.listen(PORT, "0.0.0.0", () => {
+//     console.log("server started on 5000")
+// })
+
 const express = require("express")
-const app = express();
-const path = require("path")
+const app = express()
+
+// require('dotenv').config()
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
 const serverRoute = require("./serverRoute")
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname + "/public")))
-
-app.use("/api/", serverRoute);
+app.use("/api", serverRoute)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, "0.0.0.0", () => {
-    console.log("server started on 5000")
+app.listen(PORT, () => {
+    console.log("Server is running....")
 })
