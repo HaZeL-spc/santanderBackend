bfe63f172233048e897e1890d3abcfce14092bf6
unknown <kacper.leszczu8@gmail.com>
2023-05-28 02:59:43 +0200

    testing

--- index.js
+++ index.js
@@ -1,81 +1,18 @@
-// // const express = require("express")
-// // const app = express();
-// // const path = require("path")
-// // const serverRoute = require("./serverRoute")
+const express = require("express")
+const app = express();
+const path = require("path")
+const serverRoute = require("./serverRoute")
 
-// // app.use(express.json())
-// // app.use(express.urlencoded({extended: false}))
-// // app.use(express.static(path.join(__dirname + "/public")))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+// app.use(express.static(path.join(__dirname + "/public")))
 
-// // app.use("/api/", serverRoute);
+app.use("/api/", serverRoute);
 
-// // const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
-// // app.listen(PORT, "0.0.0.0", () => {
-// //     console.log("server started on 5000")
-// // })
+app.listen(PORT, () => {
+    console.log("server started on 5000")
+})
 
-// // const express = require("express")
-// const app = express()
-
-// // require('dotenv').config()
-
-// app.use(express.urlencoded({extended: false}))
-// app.use(express.json())
-
-// const serverRoute = require("./serverRoute")
-
-// app.use("/api", serverRoute)
-
-// const PORT = process.env.PORT || 5000
-
-// app.listen(PORT, () => {
-//     console.log("Server is running....")
-// })
-
-const express = require("express");
-const { Router } = require("express");
-const router = Router();
-const db = require("./database");
-
-router.use((req, res, next) => {
-  console.log("request made to server route");
-  next();
-});
-
-router.get("/", async (req, res) => {
-  try {
-    // Create a new MySQL connection instance
-    const connection = mysql.createConnection({
-      host: 'bvjx9vxnnapywt8oi7oh-mysql.services.clever-cloud.com',
-      user: 'utpprvmh23np6jwe',
-      password: 'tcOqn2tDVFbd36xtj8o2',
-      database: 'bvjx9vxnnapywt8oi7oh',
-      port: 3306
-    });
-
-    // Connect to the MySQL server
-    console.log("0");
-    await connection.connect();
-    console.log("1")
-    res.status(500).send("Internal Server Error after connect");
-
-    // Execute your query
-    const query = "SELECT S.id, G.nazwa as nazwaGrupy, S.imie, S.nazwa as nazwisko FROM STUDENT S LEFT JOIN GRUPA G ON S.id_grupa = G.id";
-    const [results] = await connection.promise().query(query);
-    console.log("2")
-    res.status(500).send("Internal Server Error after query");
-
-
-    // Close the connection
-    connection.end();
-    console.log("3")
-
-    res.status(200).send(results);
-  } catch (error) {
-    console.error("Error executing query:", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-module.exports = router;
+module.exports = app;
--- serverRoute.js
+++ serverRoute.js
@@ -7,49 +7,52 @@ router.use((req, res, next) => {
     next()
 })
 
-router.get("/", async (req, res) => {
-    const query = "SELECT S.id, G.nazwa as nazwaGrupy, S.imie, S.nazwa as nazwisko FROM STUDENT S " + 
-    " LEFT JOIN GRUPA G ON S.id_grupa = G.id "
+// router.get("/", async (req, res) => {
+//     const query = "SELECT S.id, G.nazwa as nazwaGrupy, S.imie, S.nazwa as nazwisko FROM student S " + 
+//     " LEFT JOIN grupa G ON S.id_grupa = G.id "
+//     const results = await db.promise().query(query);
+
+//     console.log(results[0])
+//     res.status(200).send(results[0])
+// })
+
+router.get("/getstudentsgroup", async (req, res) => {
+    console.log("in /");
+    const query = "SELECT id_student, G.nazwa as nazwaGrupy, P.temat, S.imie, S.nazwa FROM student S" + 
+        " JOIN grupa G ON S.id_grupa = G.id " +
+        " JOIN projekt P ON P.id_student = S.id"
     const results = await db.promise().query(query);
 
     res.status(200).send(results[0])
 })
 
-// router.get("/getstudentsgroup", async (req, res) => {
-//     console.log("in /");
-//     const query = "SELECT id_student, G.nazwa as nazwaGrupy, P.temat, S.imie, S.nazwa FROM STUDENT S" + 
-//         " JOIN GRUPA G ON S.id_grupa = G.id " +
-//         " JOIN PROJEKT P ON P.id_student = S.id"
-//     const results = await db.promise().query(query);
+router.get("/getstudentsgroupnull", async (req, res) => {
+    console.log("in /");
+    const query = "SELECT S.id, G.nazwa as nazwaGrupy, S.imie, S.nazwa as nazwisko FROM student S " + 
+        " LEFT JOIN grupa G ON S.id_grupa = G.id "
+    const results = await db.promise().query(query);
 
-//     res.status(200).send(results[0])
-// })
+    console.log(results[0])
 
-// router.get("/getstudentsgroupnull", async (req, res) => {
-//     console.log("in /");
-//     const query = "SELECT S.id, G.nazwa as nazwaGrupy, S.imie, S.nazwa as nazwisko FROM STUDENT S " + 
-//         " LEFT JOIN GRUPA G ON S.id_grupa = G.id "
-//     const results = await db.promise().query(query);
+    res.status(200).send(results[0])
+})
 
-//     res.status(200).send(results[0])
-// })
+router.get("/getprowadzacyprojekt", async (req, res) => {
+    console.log("in /");
+    const query = "SELECT P.id, P.nazwisko, P.imie, PRO.temat  FROM prowadzacy P " + 
+        " JOIN projekt PRO ON PRO.id_prowadzacy = P.id"
+    const results = await db.promise().query(query);
 
-// router.get("/getprowadzacyprojekt", async (req, res) => {
-//     console.log("in /");
-//     const query = "SELECT P.id, P.nazwisko, P.imie, PRO.temat  FROM PROWADZACY P " + 
-//         " JOIN PROJEKT PRO ON PRO.id_prowadzacy = P.id"
-//     const results = await db.promise().query(query);
+    res.status(200).send(results[0])
+})
 
-//     res.status(200).send(results[0])
-// })
+router.get("/getstudentoversrednia", async (req, res) => {
+    console.log("in /");
+    const query = "SELECT S.id, S.imie, S.nazwa as nazwisko, S.srednia FROM student S " + 
+        " WHERE S.srednia > (SELECT AVG(srednia) from student)"
+    const results = await db.promise().query(query);
 
-// router.get("/getstudentoversrednia", async (req, res) => {
-//     console.log("in /");
-//     const query = "SELECT S.id, S.imie, S.nazwa as nazwisko, S.srednia FROM STUDENT S " + 
-//         " WHERE S.srednia > (SELECT AVG(srednia) from STUDENT)"
-//     const results = await db.promise().query(query);
-
-//     res.status(200).send(results[0])
-// })
+    res.status(200).send(results[0])
+})
 
 module.exports = router
