260626f34bd209ec08137a3981b1fe46553baee7
unknown <kacper.leszczu8@gmail.com>
2023-05-28 02:25:49 +0200

    changed port

--- database.js
+++ database.js
@@ -1,18 +1,9 @@
 const mysql = require("mysql2")
 
-const pool = mysql.createPool({
+module.exports = mysql.createConnection({
     host: 'bvjx9vxnnapywt8oi7oh-mysql.services.clever-cloud.com',
     user: 'utpprvmh23np6jwe',
     password: 'tcOqn2tDVFbd36xtj8o2',
     database: 'bvjx9vxnnapywt8oi7oh',
-    waitForConnection: true,
-    queueLimit: 0,
-    connectionLimit: 10
+    port: 3306
 })
-
-pool = mysql.getConnection((err, conn) => {
-    if (err) console.log(err);
-    console.log("connected success");
-})
-
-module.exports = pool.promise();
