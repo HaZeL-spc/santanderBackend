4503a5c1d87f17ecf8fce02667277302b6b9d3f7
unknown <kacper.leszczu8@gmail.com>
2023-05-28 02:16:50 +0200

    testing

--- database.js
+++ database.js
@@ -1,8 +1,18 @@
 const mysql = require("mysql2")
 
-module.exports = mysql.createConnection({
+const pool = mysql.createPool({
     host: 'bvjx9vxnnapywt8oi7oh-mysql.services.clever-cloud.com',
     user: 'utpprvmh23np6jwe',
     password: 'tcOqn2tDVFbd36xtj8o2',
-    database: 'bvjx9vxnnapywt8oi7oh'
+    database: 'bvjx9vxnnapywt8oi7oh',
+    waitForConnection: true,
+    queueLimit: 0,
+    connectionLimit: 10
 })
+
+pool = mysql.getConnection((err, conn) => {
+    if (err) console.log(err);
+    console.log("connected success");
+})
+
+module.exports = pool.promise();
